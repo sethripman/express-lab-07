07995952f7b424902eb1d631b01fd6ad69c58173
sethripman <sethripman@gmail.com>
2019-12-10 16:12:32 -0800

    wrote delete route test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -121,4 +121,32 @@ describe('app routes', () => {
         });
       });
   });
+
+  it('deletes a recipe by id', async() => {
+    const recipe = await Recipe.create({
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ],
+    });
+
+    return request(app)
+      .delete(`/api/v1/recipes/${recipe._id}`)
+      .then(deletedRecipe => {
+        expect(deletedRecipe.body).toEqual({
+          _id: expect.any(String),
+          name: 'cookies',
+          directions: [
+            'preheat oven to 375',
+            'mix ingredients',
+            'put dough on cookie sheet',
+            'bake for 10 minutes'
+          ],
+          __v: 0
+        });
+      });
+  });
 });
